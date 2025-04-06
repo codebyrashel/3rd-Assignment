14387be6c93dab7a87add3fe72301a3ff2b42b74
unknown <codebyrashel@gmail.com>
2025-04-06 15:40:41 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+    // window.addEventListener('DOMContentLoaded', () => {
+    //     const picture = document.querySelector('.slider-track picture');
+    //     const logos = picture.children;
+
+    //     // Clone each logo and append to the same <picture>
+    //     for (let i = 0; i < logos.length; i++) {
+    //         const clone = logos[i].cloneNode(true);
+    //         picture.appendChild(clone);
+    //     }
+    // });
